d61547edaabefa2868d594bb93263a2985c33806
scohe001 <scohe001@ucr.edu>
2022-04-06 21:22:37 -0500

    Added tap for definition and prevent lookup until input on the word is done

--- src/app/main-game/word-box/word-box.component.ts
+++ src/app/main-game/word-box/word-box.component.ts
@@ -37,6 +37,8 @@ export class WordBoxComponent implements OnInit {
       && value[value.length - 1] == value[value.length - 2]
       && value[value.length - 1] === ' ')
       { return; }
+    
+    if(value === this.textEntered) { return; }
 
     this.textEntered = value;
     let lowerWord: string = value.toLowerCase();
@@ -76,13 +78,13 @@ export class WordBoxComponent implements OnInit {
   private parseWords(value: string) {
     // Deep copy
     let oldWords: Array<Word> = JSON.parse(JSON.stringify(this.words));
-    var newWords: Array<Word>;
 
     this.words = []; // Clear what's already in there
 
     var values: Array<string> = value.split(' ');
 
-    values.forEach(word => {
+    values.forEach((word, indx) => {
+      word = word.split('').reduce((finalWord, currLetter) => finalWord += Word.isWordChar(currLetter) ? currLetter : '', '');
       if(word.length === 0) { return; }
 
       var newWord: Word = new Word(word, this.dictionaryManager);
@@ -91,15 +93,17 @@ export class WordBoxComponent implements OnInit {
         return w.word === newWord.word;
       })
 
-      if(oldWord) {
+      if(oldWord && oldWord.isGood !== WordState.PENDING_INPUT) {
         newWord.isGood = oldWord.isGood;
         newWord.definition = oldWord.definition;
-      } else {
+      } else if (indx + 1 < values.length) {
         newWord.lookupWord();
+      } else {
+        newWord.isGood = WordState.PENDING_INPUT;
+        newWord.definition = "Waiting for input to end. Add a space to force a lookup operation.";
       }
 
       this.words.push(newWord);
-      // this.words.push(new Word(word, this.dictionaryManager));
     });
 
     if(this.words.length === 0 || values[values.length - 1].length === 0) {
@@ -112,14 +116,29 @@ export class WordBoxComponent implements OnInit {
     //   this.words[this.words.length - 1].isGood = true;
     // }
 
-    console.log(this.words);
+    console.log("Finishing parse", this.words);
   }
+
+  public rowClicked(word: Word) {
+    console.log(word);
+
+    const dialogRef = this.dialog.open(DefinitionDialogue, {
+      width: '250px',
+      data: word,
+    });
+  }
+}
+
+export enum WordState {
+  GOOD,
+  DEF_NOT_FOUND,
+  PENDING_INPUT,
 }
 
 export class Word {
   public word: string;
   public definition: string;
-  public isGood: boolean;
+  public isGood: WordState;
   public score: number;
 
   constructor(word: string,
@@ -127,7 +146,7 @@ export class Word {
     // console.log("Constructor called with: " + word);
     this.word = word;
     this.definition = '';
-    this.isGood = true;
+    this.isGood = WordState.PENDING_INPUT;
     this.score = 0;
 
     this.recalcScore();
@@ -138,9 +157,9 @@ export class Word {
     this.score = 0;
 
     for (let index = 0; index < lowerWord.length; index++) {
-      if(!this.isAlpha(lowerWord[index])) { continue; }
+      if(!Word.isAlpha(lowerWord[index])) { continue; }
 
-      if(this.isVowel(lowerWord[index])) {
+      if(Word.isVowel(lowerWord[index])) {
         this.score -= lowerWord[index].charCodeAt(0) - 'a'.charCodeAt(0) + 1;
       } else {
         this.score += lowerWord[index].charCodeAt(0) - 'a'.charCodeAt(0) + 1;
@@ -160,20 +179,24 @@ export class Word {
         // console.log(r.meanings);
         // console.log(r.meanings[0].definitions);
         this.definition = r[0].meanings[0].definitions[0].definition;
-        this.isGood = true;
+        this.isGood = WordState.GOOD;
       })
       .catch((err: any) => {
         this.definition = 'Word not found.';
-        this.isGood = false;
+        this.isGood = WordState.DEF_NOT_FOUND;
         console.log(err);
       });
   }
 
-  private isAlpha(val: string) {
+  public static isAlpha(val: string) {
     return  "abcdefghijklmnopqrstuvwxyz".includes(val);
   }
 
-  private isVowel(val: string) {
+  public static isVowel(val: string) {
     return "aeiou".includes(val);
+  }
+
+  public static isWordChar(val: string) {
+    return this.isAlpha(val) || val === "'";
   }
 }
